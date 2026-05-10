28b5481eb2bac269ef5daee72b6fe751015f92ff
shahdtarek2005 <shahdsleem58@gmail.com>
2026-05-10 20:00:52 +0300

    Update Mavenproject15.java

--- mavenproject15/src/main/java/com/mycompany/mavenproject15/Mavenproject15.java
+++ mavenproject15/src/main/java/com/mycompany/mavenproject15/Mavenproject15.java
@@ -11,40 +11,43 @@ package com.mycompany.mavenproject15;
 public class Mavenproject15 {
 
     public static void main(String[] args) {
-        BinarySearchh tree = new BinarySearchh();
+        BinarySearchh bst = new BinarySearchh();
+
+        bst.insert(105, "shahd", 3.8);
+        bst.insert(102, "sleem", 2.2);
+        bst.insert(110, "wael", 1.9);
+        bst.insert(101, "jomana", 3.5);
+        bst.insert(109, "zaki", 1.5);
+        bst.insert(104, "lotfy", 2.9);
+        bst.insert(108, "adham", 3.2);
+        bst.insert(103, "Hana", 1.8);
+        bst.insert(107, "Islam", 3.0);
+        bst.insert(106, "khaled", 2.5);
+        bst.insert(112, "fatma", 1.2);
+        bst.insert(111, "mahmoud", 3.9);
+
+       
+        bst.display();
+        System.out.println("Size: " + bst.size());
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
         
-        tree.insert(10, "ali", 3.2);
-        tree.insert(5, "sara", 2.8);
-        tree.insert(15, "omar", 3.9);
-        tree.insert(2, "mona", 2.5);
-        tree.insert(7, "khaled", 3.0);
-
-        
-        System.out.println("all Students:");
-        tree.display();
-
-      
-        System.out.println("Search for ID 5:");
-        Student found = tree.search(5);
-
-        if (found != null) {
-            found.display();
-        } else {
-            System.out.println("Not found");
-        }
-
-        
-        System.out.println("tree size: " + tree.size());
-
-        
-        System.out.println("lowest GPA students:");
-        tree.bottom10();
+        bst.bottom10();
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
+        bst.bottom5();
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
+        bst.separateGPAUnder2();
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
 
         
-        System.out.println("after removing ID 5:");
-        tree.remove(5);
-        tree.display();
+        System.out.println("Removing ID 104: " + bst.remove(104));
+        bst.display();
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("New size: " + bst.size());
+
+       
+        System.out.println("Search for ID 110: " + bst.search(110));
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("Search for ID 999: " + bst.search(999));
     }
 }
-    
 
